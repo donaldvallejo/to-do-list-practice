c6598812a2e84a413d29382abf68967129f33234
Donald Vallejo <localscrub@Donalds-MacBook-Pro.local>
2018-10-27 10:48:30 -0700

    after os update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 
-
+const Todo = (props) => (
+  <div className="todoPiece">
+   {props.todo.text}
+   <button>Done</button>
+  </div>
+)
 class App extends Component {
   
-  state = { todoList : [{
-    text : "Learn React",
-    done : false
-  }] }
-  
+  state = { 
+    todoList : [
+      {
+        text: 'Be awesome',
+        done: true,
+      },
+      {
+        text : "Learn React",
+        done : false,
+      },
+      {
+        text: 'Learn something else',
+        done: false,
+      },
+    ],
+  }
+
   render() {
+    const todoList = this.state.done
+    console.log(this.state)
     return (
       <div className="App">
       <h1>TO DO List</h1>
       <div className="todoList">
-
+      {this.state.todoList.map((todo,index) => <Todo todo={todo} key={index} />)}    
       </div>
-        
+      
+      <div className="textField">
+        <input className="textfeild"></input>
+        <button onClick={() => console.log("yo")} >Add</button>
+      </div> 
       </div>
     );
   }
